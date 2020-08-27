0ce25bc80a2a77dca6d0a797da60f5edc6e21f67
Phanstal <49861571+Phanstal@users.noreply.github.com>
2020-08-27 11:26:27 +0800

    Add files via upload

---
+++ Source/NMMProject/NMMProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NMMProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NMMPROJECT_API ANMMProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
